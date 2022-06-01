7ca8dc519c06b76ac3618bb06404dc1704acd511
kameswaren04 <kameswaren.vn@fibonalabs.com>
2022-06-01 19:40:39 +0530

    card done

--- assign.js
+++ assign.js
@@ -10,26 +10,20 @@ $(document).ready(function(){
 function loadProducts(data){
 
     var data1 = document.createElement('div');
-   data1.innerHTML =  '<div class="rect">'+
+   data1.innerHTML =   '<div class="rect">'+
     '<div class="rect1"><img src="'+ data.EmployeeDP +'" class="rect_img"></div>'+
     '<div class="subrect"><p class="subrect_p1">'+ data.EmployeeName +'</p>'+
     '<span>'+ data.Designation +'</span>'+
     '<p class="subrect_p2">'+ data.EmployeeDetails +'</p>'+
-'</div>';
+'</div>'+'</div>'+ '<div class="hoverdis">'+data.htext +'<div class="bright">'+
+'<button class="btn btn2">'+data.view+'</button>'+'</div>'+'</div>';
 document.getElementById("content1").appendChild(data1);
-//console.log(data1);
 }
 
-const division = document.getElementsByClassName("rect");
-const hide = document.getElementsByClassName("hide");
 
-division.addEventListner("mouseenter",a);
-division.addEventListner("mouseleave",b);
-    
-function a(){
-     
-}
 
-function b(){
 
-}
+
+
+
+
